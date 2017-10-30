03467f1fc2b422826e2f5dda95aba797433c1c93
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-30 12:29:36 +0600

    java-commons Resource used

--- beans/src/main/java/tech/lapsa/insurance/notifier/beans/NotificationTemplates.java
+++ beans/src/main/java/tech/lapsa/insurance/notifier/beans/NotificationTemplates.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.Locale;
 
 import tech.lapsa.java.commons.localization.LocalizedElement;
+import tech.lapsa.java.commons.resources.Resources;
 
 public enum NotificationTemplates implements LocalizedElement {
     NEW_CASCO_COMPANY_EMAIL_TEMPLATE, //
@@ -15,29 +16,6 @@ public enum NotificationTemplates implements LocalizedElement {
     ;
 
     public InputStream getResourceAsStream(Locale locale) {
-
-	InputStream result = null;
-	String name = regular(locale);
-
-	while (name.startsWith("/")) {
-	    name = name.substring(1);
-	}
-
-	ClassLoader classLoader = Thread.currentThread()
-		.getContextClassLoader();
-
-	if (classLoader == null) {
-	    classLoader = this.getClass().getClassLoader();
-	    result = classLoader.getResourceAsStream(name);
-	} else {
-	    result = classLoader.getResourceAsStream(name);
-
-	    if (result == null) {
-		classLoader = this.getClass().getClassLoader();
-		if (classLoader != null)
-		    result = classLoader.getResourceAsStream(name);
-	    }
-	}
-	return result;
+	return Resources.getAsStream(this.getClass(), regular(locale));
     }
 }
